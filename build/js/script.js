ea18d2f2daaeb8c5f92fb8cc24dc874c32634cba
raulmahya <raulmahya11@gmail.com>
2023-10-30 20:25:35 +0700

    added : get all for graphic

--- build/js/script.js
+++ build/js/script.js
@@ -39,184 +39,262 @@ const colors = {
   primaryDarker: cssColors(`--color-${getColor()}-darker`),
 }
 
-const barChart = new Chart(document.getElementById('barChart'), {
-  type: 'bar',
-  data: {
-    labels: months,
-    datasets: [
-      {
-        data: randomData(),
-        backgroundColor: colors.primary,
-        hoverBackgroundColor: colors.primaryDark,
+// Mengambil data dari API
+fetch('https://us-central1-testlogin-366704.cloudfunctions.net/allprod')
+  .then(response => response.json())
+  .then(data => {
+    // Mengubah data API sesuai kebutuhan untuk grafik
+    const products = data.map(item => ({
+      name: item.name,
+      price: item.price,
+      stock: item.stock,
+    }));
+
+    // Mengambil label dan value dari data produk
+    const productNames = products.map(product => product.name);
+    const productPrices = products.map(product => product.price);
+
+    // Membuat grafik Bar Chart dengan data yang sudah diambil
+   new Chart(document.getElementById('barChart'), {
+      type: 'bar',
+      data: {
+        labels: productNames,
+        datasets: [
+          {
+            data: productPrices,
+            backgroundColor: colors.primary,
+            hoverBackgroundColor: colors.primaryDark,
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
+      options: {
+        scales: {
+          yAxes: [
+            {
+              gridLines: false,
+              ticks: {
+                beginAtZero: true,
+                stepSize: 50,
+                fontSize: 12,
+                fontColor: '#97a4af',
+                fontFamily: 'Open Sans, sans-serif',
+                padding: 10,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: false,
+              ticks: {
+                fontSize: 12,
+                fontColor: '#97a4af',
+                fontFamily: 'Open Sans, sans-serif',
+                padding: 5,
+              },
+              categoryPercentage: 0.5,
+              maxBarThickness: '10',
+            },
+          ],
+        },
+        cornerRadius: 2,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+      },
+    });
+  })
+  .catch(error => {
+    console.error('Ada masalah saat mengambil data:', error);
+    // Tindakan penanganan kesalahan jika diperlukan
+  });
+
+
+fetch('https://asia-southeast2-testlogin-366704.cloudfunctions.net/getaaalll')
+.then(response => response.json())
+.then(data => {
+  const labels = data.map(entry => entry.username);
+  const values = data.map(entry => entry.password.length);
+  new Chart(document.getElementById('doughnutChart'), {
+    type: 'doughnut',
+    data: {
+      labels: labels,
+      datasets: [
         {
-          gridLines: false,
+          data: values,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)', // Warna pertama
+            'rgba(54, 162, 235, 0.7)', // Warna kedua
+            'rgba(255, 206, 86, 0.7)' // Warna ketiga
+            // ...Tambahkan warna lebih banyak jika diperlukan
+          ],
+          hoverBackgroundColor: 'lightblue', // Warna saat hover (ganti sesuai kebutuhan)
+          borderWidth: 0,
+          weight: 0.5,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: false,
+      },
+      animation: {
+        animateScale: true,
+        animateRotate: true,
+      },
+    },
+  });
+})
+.catch(error => {
+  console.error('Ada masalah saat mengambil data:', error);
+  // Tindakan penanganan kesalahan jika diperlukan
+});
+
+// Mengambil data dari API
+fetch('https://us-central1-testlogin-366704.cloudfunctions.net/getAllcommned')
+  .then(response => response.json())
+  .then(data => {
+    // Mengubah data API sesuai kebutuhan untuk grafik
+    const comments = data.data.map(item => ({
+      username: item.username,
+      commentLength: item.comment.length, // Menggunakan panjang komentar sebagai contoh data grafik
+    }));
+
+    // Mengambil label dan value dari data komentar
+    const usernames = comments.map(comment => comment.username);
+    const commentLengths = comments.map(comment => comment.commentLength);
+
+    // Membuat grafik Bar Chart dengan data yang sudah diambil
+    new Chart(document.getElementById('activeUsersChart'), {
+      type: 'bar',
+      data: {
+        labels: usernames,
+        datasets: [
+          {
+            data: commentLengths,
+            backgroundColor: colors.primary,
+            borderWidth: 0,
+            categoryPercentage: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              display: false,
+              gridLines: false,
+            },
+          ],
+          xAxes: [
+            {
+              display: false,
+              gridLines: false,
+            },
+          ],
           ticks: {
-            beginAtZero: true,
-            stepSize: 50,
-            fontSize: 12,
-            fontColor: '#97a4af',
-            fontFamily: 'Open Sans, sans-serif',
             padding: 10,
           },
         },
-      ],
-      xAxes: [
-        {
-          gridLines: false,
-          ticks: {
-            fontSize: 12,
-            fontColor: '#97a4af',
-            fontFamily: 'Open Sans, sans-serif',
-            padding: 5,
-          },
-          categoryPercentage: 0.5,
-          maxBarThickness: '10',
-        },
-      ],
-    },
-    cornerRadius: 2,
-    maintainAspectRatio: false,
-    legend: {
-      display: false,
-    },
-  },
-})
-
-const doughnutChart = new Chart(document.getElementById('doughnutChart'), {
-  type: 'doughnut',
-  data: {
-    labels: ['Oct', 'Nov', 'Dec'],
-    datasets: [
-      {
-        data: [random(), random(), random()],
-        backgroundColor: [colors.primary, colors.primaryLighter, colors.primaryLight],
-        hoverBackgroundColor: colors.primaryDark,
-        borderWidth: 0,
-        weight: 0.5,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-      position: 'bottom',
-    },
-
-    title: {
-      display: false,
-    },
-    animation: {
-      animateScale: true,
-      animateRotate: true,
-    },
-  },
-})
-
-const activeUsersChart = new Chart(document.getElementById('activeUsersChart'), {
-  type: 'bar',
-  data: {
-    labels: [...randomData(), ...randomData()],
-    datasets: [
-      {
-        data: [...randomData(), ...randomData()],
-        backgroundColor: colors.primary,
-        borderWidth: 0,
-        categoryPercentage: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
+        cornerRadius: 2,
+        maintainAspectRatio: false,
+        legend: {
           display: false,
-          gridLines: false,
         },
-      ],
-      xAxes: [
-        {
-          display: false,
-          gridLines: false,
+        tooltips: {
+          prefix: 'Users',
+          bodySpacing: 4,
+          footerSpacing: 4,
+          hasIndicator: true,
+          mode: 'index',
+          intersect: true,
         },
-      ],
-      ticks: {
-        padding: 10,
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
       },
-    },
-    cornerRadius: 2,
-    maintainAspectRatio: false,
-    legend: {
-      display: false,
-    },
-    tooltips: {
-      prefix: 'Users',
-      bodySpacing: 4,
-      footerSpacing: 4,
-      hasIndicator: true,
-      mode: 'index',
-      intersect: true,
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: true,
-    },
-  },
-})
+    });
+  })
+  .catch(error => {
+    console.error('Ada masalah saat mengambil data:', error);
+    // Tindakan penanganan kesalahan jika diperlukan
+  });
 
-const lineChart = new Chart(document.getElementById('lineChart'), {
-  type: 'line',
-  data: {
-    labels: months,
-    datasets: [
-      {
-        data: randomData(),
-        fill: false,
-        borderColor: colors.primary,
-        borderWidth: 2,
-        pointRadius: 0,
-        pointHoverRadius: 0,
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    scales: {
-      yAxes: [
-        {
-          gridLines: false,
-          ticks: {
-            beginAtZero: false,
-            stepSize: 50,
-            fontSize: 12,
-            fontColor: '#97a4af',
-            fontFamily: 'Open Sans, sans-serif',
-            padding: 20,
+
+// Mengambil data dari API
+fetch('https://asia-southeast2-testlogin-366704.cloudfunctions.net/bismillahcontentall')
+  .then(response => response.json())
+  .then(data => {
+    // Mengubah data API sesuai kebutuhan untuk grafik
+    const contents = data.map(item => ({
+      ID: item.ID,
+      content: item.content.length, // Menyesuaikan contoh penggunaan panjang konten sebagai data grafik
+    }));
+
+    // Mengambil label dan value dari data konten
+    const contentIDs = contents.map(content => content.ID);
+    const contentLengths = contents.map(content => content.content);
+
+    // Membuat grafik Line Chart dengan data yang sudah diambil
+     new Chart(document.getElementById('lineChart'), {
+      type: 'line',
+      data: {
+        labels: contentIDs,
+        datasets: [
+          {
+            data: contentLengths,
+            fill: false,
+            borderColor: colors.primary,
+            borderWidth: 2,
+            pointRadius: 0,
+            pointHoverRadius: 0,
           },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              gridLines: false,
+              ticks: {
+                beginAtZero: false,
+                stepSize: 50,
+                fontSize: 12,
+                fontColor: '#97a4af',
+                fontFamily: 'Open Sans, sans-serif',
+                padding: 20,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: false,
+            },
+          ],
         },
-      ],
-      xAxes: [
-        {
-          gridLines: false,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
         },
-      ],
-    },
-    maintainAspectRatio: false,
-    legend: {
-      display: false,
-    },
-    tooltips: {
-      hasIndicator: true,
-      intersect: false,
-    },
-  },
-})
+        tooltips: {
+          hasIndicator: true,
+          intersect: false,
+        },
+      },
+    });
+  })
+  .catch(error => {
+    console.error('Ada masalah saat mengambil data:', error);
+    // Tindakan penanganan kesalahan jika diperlukan
+  });
+
 
 let randomUserCount = 0
 
